d957a6589f7642889160d61cb811a7778349fabc
František Petko <frantisek.petko7@gmail.com>
2025-01-07 16:20:29 +0100

    update

--- scaffold/src/main.ts
+++ scaffold/src/main.ts
@@ -26,14 +26,6 @@ async function bootstrap() {
 	});
 	app.setGlobalPrefix('/api');
 
-
-	/*
-	logger.log(process.env.PROJECT_URL);
-	logger.debug(process.env.NODE_ENV);
-	logger.warn(process.cwd());
-	*/
-
-
 	await app.listen(port, () =>
 		logger.log(`App is listening at http://localhost:${port}`),
 	);
